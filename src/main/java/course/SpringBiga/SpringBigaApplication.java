2bf51114269f41631467624d196b4f11f2af6de6
Bighetto <arthurbighetto36@gmail.com>
2021-08-05 17:45:28 -0300

    Primeira versao

---
+++ src/main/java/course/SpringBiga/SpringBigaApplication.java
@@ -0,0 +1,15 @@
+package course.SpringBiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBigaApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/course/SpringBiga/SpringBigaApplicationTests.java
@@ -0,0 +1,13 @@
+package course.SpringBiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
