d256cce0b5fe09622975f903a8e660df6b447d47
Bighetto <arthurbighetto36@gmail.com>
2021-08-06 16:34:11 -0300

    Operacao de instanciacao

--- src/main/java/course/SpringBiga/SpringBigaApplication.java
+++ src/main/java/course/SpringBiga/SpringBigaApplication.java
@@ -1,12 +1,30 @@
 package course.SpringBiga;
 
+import course.SpringBiga.Domain.Categoria;
+import course.SpringBiga.Repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBigaApplication {
+public class SpringBigaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBigaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
