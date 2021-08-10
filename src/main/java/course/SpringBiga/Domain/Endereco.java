62b287331e58041e850a87daf236ece3fff20112
Bighetto <arthurbighetto36@gmail.com>
2021-08-10 15:28:54 -0300

    endpoints clientes/{id}

--- src/main/java/course/SpringBiga/Domain/Endereco.java
+++ src/main/java/course/SpringBiga/Domain/Endereco.java
@@ -1,6 +1,8 @@
 package course.SpringBiga.Domain;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
@@ -19,6 +21,7 @@ public class Endereco implements Serializable {
     private String cep;
 
 
+    @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
---
+++ src/main/java/course/SpringBiga/Services/ClienteService.java
@@ -0,0 +1,25 @@
+package course.SpringBiga.Services;
+
+import course.SpringBiga.Domain.Cliente;
+import course.SpringBiga.Repositories.ClienteRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+
+    public Cliente buscar(Integer id) throws ObjectNotFoundException {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+        }
+
+    }
+
